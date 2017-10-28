8ce43f2d76652df3ebdee4a9d91f6dd1d31693cb
徐昊 <xuhao@mucang.cn>
2017-10-29 01:50:33 +0800

    修改ignore

---
+++ app/src/main/java/shadows/android/shadowswalker/MainActivity.java
@@ -0,0 +1,12 @@
+package shadows.android.shadowswalker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
